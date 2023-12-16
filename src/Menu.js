c132f7aff1691241b00757c58ea6e7f4e38f68f9
Veronelli <facu2000veronelli@outlook.com>
2023-12-16 10:59:46 -0300

    add navlink

--- src/Menu.js
+++ src/Menu.js
@@ -1,11 +1,39 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-function Menu(){
-    return(
-        <>
-            <nav>Menu</nav>
-        </>
-    )
+const routes = [
+  {
+    to: "/",
+    text: "Home",
+  },
+  {
+    to: "/blog",
+    text: "Blog",
+  },
+  {
+    to: "/profile",
+    text: "Profile",
+  },
+];
+
+function Menu() {
+  return (
+    <>
+      <nav>
+        <ul>
+          {routes.map((route) => (
+            <NavLink
+              to={route.to}
+              key={`route-${route.to}`}
+              style={({isActive}) => ({ color: isActive ? "red" : "black" })}
+            >
+              {route.text}
+            </NavLink>
+          ))}
+        </ul>
+      </nav>
+    </>
+  );
 }
 
-export {Menu};
+export { Menu };
