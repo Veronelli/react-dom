46fbe0b0aa0042ccd0d30ed51463871da610d219
Veronelli <facu2000veronelli@outlook.com>
2023-12-17 16:39:25 -0300

    add outlet

--- src/BlogPage.js
+++ src/BlogPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { blogData } from "./BlogData";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 function BlogPage() {
   return (
@@ -8,6 +8,7 @@ function BlogPage() {
       <h1>Blog Page</h1>
 
       <ul>
+        <Outlet/>
         {blogData.map((blog) => (
           <li key={`blogData-${blog.slug}`}>
             <Link to={blog.slug}>
