15bcf54c4f67d09f49efbce236134eba0a53a2e1
Veronelli <facu2000veronelli@outlook.com>
2023-12-17 16:29:04 -0300

    navigation

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,13 +1,15 @@
 import React, { useEffect } from "react";
 import { blogData } from "./BlogData";
 import { useParams } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 function BlogPost() {
   const { slug } = useParams();
   const postData = blogData.find((post) => post.slug === slug);
-
+  const navigate = useNavigate();
   return (
     <>
+      <button onClick={() => navigate(-1)}>Back</button>
       <h1>{postData.title}</h1>
       <h3>{postData.content}</h3>
     </>
