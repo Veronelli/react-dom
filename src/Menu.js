74a4f7c374d341df134378a6229a65d621b5a843
Veronelli <facu2000veronelli@outlook.com>
2023-12-17 22:22:20 -0300

    hide options

--- src/Menu.js
+++ src/Menu.js
@@ -1,43 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { useAuth } from "./auth";
 
 const routes = [
   {
     to: "/",
     text: "Home",
+    private: false,
   },
   {
     to: "/blog",
     text: "Blog",
+    private: false,
   },
   {
     to: "/profile",
     text: "Profile",
+    private: true,
   },
   {
     to: "/login",
-    text: "Login"
+    text: "Login",
+    private: false,
+    showLoged: true
   },
   {
     to: "/logout",
-    text: "Logout"
-  }
+    text: "Logout",
+    private: false,
+  },
 ];
 
 function Menu() {
+  const {auth} = useAuth();
   return (
     <>
       <nav>
         <ul>
-          {routes.map((route) => (
-            <NavLink
-              to={route.to}
-              key={`route-${route.to}`}
-              style={({isActive}) => ({ color: isActive ? "red" : "black" })}
-            >
-              {route.text}
-            </NavLink>
-          ))}
+          {routes.map((route) => {
+            if((route.private && !auth.user) || (route.showLoged && !!auth.user))
+              return null;
+            
+            return (
+              <NavLink
+                to={route.to}
+                key={`route-${route.to}`}
+                style={({ isActive }) => ({
+                  color: isActive ? "red" : "black",
+                })}
+              >
+                {route.text}
+              </NavLink>
+            );
+          })}
         </ul>
       </nav>
     </>
